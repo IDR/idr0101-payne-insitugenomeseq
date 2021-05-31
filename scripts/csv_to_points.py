c4121837b98635140a6b3f3a7db33154ce97af5f
William Moore <w.moore@dundee.ac.uk>
2021-05-31 15:55:47 +0100

    csv_to_points.py handles _hyb and processed images

--- scripts/csv_to_points.py
+++ scripts/csv_to_points.py
@@ -47,21 +47,24 @@ colors = [
 ]
 
 project_name = "idr0101-payne-insitugenomeseq/experimentB"
-dataset_name = "Embryo_%02d"
-image_name = "cell%03d_processed"
 
-
-def get_image(project, embryo_id, cell_id):
+def get_dataset(project, embryo_id):
     for dataset in project.listChildren():
-        if dataset.name == dataset_name % embryo_id:
-            for image in dataset.listChildren():
-                if image.name == image_name % cell_id:
-                    return image
+        if dataset.name == "Embryo_%02d" % embryo_id:
+            return dataset
 
 
-def create_roi(updateService, image, name, shapes):
+def get_image(dataset, name_contains):
+    imgs = [image for image in dataset.listChildren() if name_contains in image.name]
+    assert len(imgs) < 2
+    if len(imgs) == 1:
+        return imgs[0]
+
+
+def create_roi(updateService, image, shapes):
     roi = omero.model.RoiI()
     # NB: $ populate metadata Image:1 --file data.csv NEEDS roi names
+    name = shapes[0].textValue.val
     roi.name = rstring(name)
     roi.setImage(image._obj)
     for shape in shapes:
@@ -107,7 +110,11 @@ def populate_metadata(image, file_path, file_name):
     ctx.parse()
 
 
-def process_image(conn, image, embryo_id, cell_id):
+def process_image(conn, image, embryo_id, cell_id=None):
+
+    if image.getROICount() > 0:
+        return
+
     updateService = conn.getUpdateService()
 
     # Read csv for each embryo
@@ -122,15 +129,28 @@ def process_image(conn, image, embryo_id, cell_id):
 
     rows_by_chr = defaultdict(list)
     max_chr = 0
+  
 
     # first, group rows by chr_id
-    # for index, row in df.iterrows():
-    for index, row in df.loc[df['cell_id'] == cell_id].iterrows():
-        chr_id = row["chr"]
-        max_chr = max(max_chr, chr_id)
-        rows_by_chr[chr_id].append(row)
+    if cell_id is None:
+        for index, row in df.iterrows():
+            # chr_id based on cell AND chr for _hybridization images
+            chr_id = (100 * row['cell_id']) + row['chr']       # e.g. 120
+            max_chr = max(max_chr, chr_id)
+            rows_by_chr[chr_id].append(row)
+    else:
+        # filter table rows by cell_id
+        for index, row in df.loc[df['cell_id'] == cell_id].iterrows():
+            chr_id = row['chr']
+            max_chr = max(max_chr, chr_id)
+            rows_by_chr[chr_id].append(row)
 
-    # Create 1 ROI for each chr
+    def get_coord(row, xyz="x"):
+        if cell_id is None:
+            return row[xyz + "_um_abs"]
+        return row[xyz + "_um"]
+
+    # Create 1 ROI for each chr (per cell)
     for chr_id in range(max_chr):
         if chr_id not in rows_by_chr:
             continue
@@ -140,25 +160,27 @@ def process_image(conn, image, embryo_id, cell_id):
         # create a Point for each row
         for row in rows_by_chr[chr_id]:
             point = omero.model.PointI()
-            point.textValue = rstring(row["chr_name"])
-            # For Processed images we use x_um, y_um, z_um
+            if cell_id is None:
+                point.textValue = rstring(f"cell{row['cell_id']}_{row['chr_name']}")
+            else:
+                point.textValue = rstring(row['chr_name'])
             # NB: switch X and Y (analysis used a different coordinate system)
-            point.y = rdouble(row["x_um"] * 9.2306)
-            point.x = rdouble(row["y_um"] * 9.2306)
-            point.theZ = rint(int(round(row["z_um"] * 2.5)))
+            point.y = rdouble(get_coord(row, 'x') * 9.2306)
+            point.x = rdouble(get_coord(row, 'y') * 9.2306)
+            point.theZ = rint(int(round(get_coord(row, 'z') * 2.5)))
             if chr_id <= len(colors):
                 point.strokeColor = rint(rgba_to_int(*colors[chr_id - 1]))
 
             points.append(point)
 
-        roi = create_roi(updateService, image, row["chr_name"], points)
+        roi = create_roi(updateService, image, points)
 
         # Need to get newly saved shape IDs
         shapes = list(roi.copyShapes())
         print("saved shapes", len(shapes))
         for row, shape in zip(rows_by_chr[chr_id], shapes):
             # checks that the order of shapes is same as order of rows
-            assert shape.theZ.val == round(row["z_um"] * 2.5)
+            assert shape.theZ.val == round(get_coord(row, 'z') * 2.5)
             row["roi"] = roi.id.val
             row["shape"] = shape.id.val
             df2 = df2.append(row)
@@ -180,16 +202,22 @@ def main(conn):
     print("Project", project.id)
     conn.SERVICE_OPTS.setOmeroGroup(project.getDetails().group.id.val)
 
-    # For each embryo, we don't know how many cells are present
-    # Simply start at 1 and keep checking until None found
     for embryo_id in range(1, 58):
+        dataset = get_dataset(project, embryo_id)
+        hyb_image = get_image(dataset, name_contains="_hyb")
+        print("Processing hyb_image", hyb_image.id, hyb_image.name)
+        process_image(conn, hyb_image, embryo_id)
+
         cell_id = 1
-        image = get_image(project, embryo_id, cell_id)
+        # process cell001_processed images
+        # For each embryo, we don't know how many cells are present
+        # Simply start at 1 and keep checking until None found
+        image = get_image(dataset, name_contains="cell%03d_processed" % cell_id)
         while image is not None:
             print("Processing image", image.id, image.name)
             process_image(conn, image, embryo_id, cell_id)
             cell_id += 1
-            image = get_image(project, embryo_id, cell_id)
+            image = get_image(dataset, name_contains="cell%03d_processed" % cell_id)
 
 # Usage:
 # cd idr0101-payne-insitugenomeseq
